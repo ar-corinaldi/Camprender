e2cd59ff5ae776a172c865627fd221e24af5d158
Dany Benavides Pedroza <danybenavidespedroza@Danys-MacBook-Pro.local>
2020-05-30 14:01:27 -0500

    List

--- front/src/Components/ATip.js
+++ front/src/Components/ATip.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './ATip.css'
 
 const ATip = (props) => {
     return (
-        <div>
-            <h1>Esta es la lista</h1>
+        <div class="container">
+                <div class="card">
+                    <h3 class="title">{props.tip.titulo}</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+                    <div class="imagen">
+                        <img src={props.tip.foto} alt="fotoTip"></img>
+                    </div>
+                </div>
+            <div class="card2">
+                <h3>{props.tip.titulo}</h3>
+            </div>
         </div>
     )
 }
--- front/src/Container/Tips.js
+++ front/src/Container/Tips.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import ATip from '../Components/ATip';
+
 
 class Tips extends Component {
+
+    state = {
+        tips: [{
+            titulo: 'Cultivo de papas',
+            descripcion: "En el campo yo realizo el cultivo de 200000 oaoas",
+            telefono: "320275888", tags: ["agricultura", "papa"],
+            foto: "https://agrotendencia.tv/agropedia/wp-content/uploads/2019/10/agrotendencia-papa-20.jpg",
+            like: 0,
+            comentarios: [{ comentario: "wow", telefono: "123456789" }]
+        },
+        {
+            titulo: 'Cultivo de Yuca',
+            descripcion: "En el campo yo reaklfsdhjfal sdlk fjslks",
+            telefono: "320275878", tags: ["agricultura", "papa"],
+            foto: "https://i.ytimg.com/vi/YTDGDPIOYfU/maxresdefault.jpg",
+            like: 0,
+            comentarios: [{ comentario: "WOOOW", telefono: "123456589" }]
+        }]
+    }
+
     render() {
+
+        let lista = null
+
+        lista = (
+            <div>
+                {
+                    this.state.tips.map((element, index) => {
+                        return (
+                            <div key={index}>
+                                <ATip style={{width:'100%'}} tip={element}></ATip>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+
         return (
             <div>
-                <h1>Esta es la lista</h1>
-            </div>)
+                {lista}
+            </div>
+        )
     }
 }
 
