9d0c9589ea33a3786e09508abdbb395ad143b7a9
Allan Corinaldi <allan9899@gmail.com>
2020-05-30 12:49:27 -0500

    crear tip

--- libs/MongoUtils.js
+++ libs/MongoUtils.js
@@ -1,10 +1,11 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
+require("dotenv").config();
 
 function MongoUtils() {
   mu = {};
-  mu.Users = {};
-  mu.Tips = {};
+  mu.users = {};
+  mu.tips = {};
 
   const ObjectId = mongodb.ObjectID;
   const DB_NAME = process.env.DB_NAME || "Error Database";
@@ -17,8 +18,25 @@ function MongoUtils() {
     return client.connect();
   };
 
+  mu.tips.findAll = () => {
+    return mu.connect().then((client) => {
+      const tips = client.db(DB_NAME).collection("Tips");
+      return tips
+        .find({})
+        .toArray()
+        .finally(() => client.close());
+    });
+  };
 
-
+  mu.tips.create = (tip) => {
+    return mu.connect().then((client) => {
+      const tips = client.db(DB_NAME).collection("Tips");
+      // console.log("En mongo utils",tip);
+      console.log({tip})
+      return tips
+        .insertOne(tip).finally(() => client.close());
+    });
+  };
   return mu;
 }
 
