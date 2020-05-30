4e808fe1955ac5ced3ecc2911fd091c61abe64d6
Allan Corinaldi <allan9899@gmail.com>
2020-05-30 11:36:57 -0500

    fixing home

--- front/src/App.js
+++ front/src/App.js
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Navbar";
+import Home from "./Home";
+
 function App() {
   const [nombre, setNombre] = useState("prueba");
   return (
     <div className="App">
-      <header className="App-header">Hello World!</header>
-      <Navbar nombre={nombre} setNombre={setNombre}></Navbar>
-      <Switch>
-        <Route path="/" component={Home}/>
-      </Switch>
+      <Router>
+        <header className="App-header">Hello World!</header>
+        <Navbar nombre={nombre} setNombre={setNombre}></Navbar>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
