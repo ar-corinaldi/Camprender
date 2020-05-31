63537a8e843acbc94ab0fc5c0cd75d2a39f1a139
Allan Corinaldi <allan9899@gmail.com>
2020-05-30 20:36:53 -0500

    ping

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Views/Navbar";
@@ -11,9 +11,16 @@ import Home from "./Views/Home";
 import CreateTips from "./Views/CreateTips";
 
 import CreateUser from "./Views/CreateUser";
+import Login from "./Views/Login";
 
 function App() {
   const [nombre, setNombre] = useState("prueba");
+  const [user, setUser] = useState(null);
+  useEffect( () => {
+    console.log(user);
+    if(user) localStorage.setItem("user",  user);
+  } , [user]);
+
   return (
     <div className="App">
       <Router>
@@ -24,6 +31,7 @@ function App() {
           <Route exact path="/tips/:tipId" component={ATip} />{" "}
           <Route exact path="/register" component={CreateTips} />{" "}
           <Route exact path="/registerUser" component={CreateUser} />{" "}
+          <Route exact path="/login" component={() => <Login user={user} setUser={setUser}/>}/>
         </Switch>
       </Router>
     </div>
