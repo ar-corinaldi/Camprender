f7ffb349079b0fe7decdbdc1961cf903206d6395
Isabela <im.sarmiento@uniandes.edu.co>
2020-05-30 22:01:49 -0500

    Dos filtros funcionales
    Filtro por region o categoria

--- front/src/Views/Tips.js
+++ front/src/Views/Tips.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ATip from "./ATip";
+import InputLabel from "@material-ui/core/InputLabel";
 
 function Tips(props) {
   // let tempState = {
@@ -29,34 +30,67 @@ function Tips(props) {
   //   ],
   // };
 
-  const [state, setState] = useState({tips:[]});
-  const [search, setSearch] = useState('');
+  const [state, setState] = useState({ tips: [] });
+
+  /*
+  Const para filtrar por region
+  */
+  const [search, setSearch] = useState("");
+
+  /*
+  Const para filtrar por categoria
+  */
+  const [search2, setSearch2] = useState("");
+
+  /*
+  Const para buscar con base en el titulo por categoria
+  */
+  const [tituloBuscado, setTituloBuscado] = useState("");
+
   const updateSearch = (event) => {
-    setSearch(event.target.value.substr(0,20));
-  }
-  let filteredTips = state.tips.filter((tip) =>{
-    return tip.region.toLowerCase().indexOf(search) !== -1;
+    setSearch(event.target.value.substr(0, 20));
+  };
+
+  const updateSearch2 = (event) => {
+    setSearch2(event.target.value.substr(0, 20));
+  };
+
+  const updateTituloBuscado = (event) => {
+    setTituloBuscado(event.target.value.substr(0, 20));
+  };
+
+  let filteredTips = state.tips.filter((tip) => {
+    //console.log("Search", search);
+    //console.log("Search2", search2);
+    var cond1 = tip.region.toLowerCase().indexOf(search) !== -1;
+    var cond2 = tip.tags[0].toLowerCase().indexOf(search2) !== -1;
+    return cond1 && cond2;
   });
-  useEffect(()=>{
-    fetching()
-  },[])
+
+  useEffect(() => {
+    fetching();
+  }, []);
 
   const fetching = async () => {
-    fetch("/tips").then((res) => res.json()).then((respuesta)=> 
-    {
-      let tempState = {};
-      tempState.tips = respuesta
-      setState(tempState);
-      console.log(state);
-
-    });
+    fetch("/tips")
+      .then((res) => res.json())
+      .then((respuesta) => {
+        let tempState = {};
+        tempState.tips = respuesta;
+        setState(tempState);
+        console.log(state);
+      });
   };
   //fetching();
 
-
   return (
     <div style={{ marginTop: "30px" }}>
-    <input type='text' value={search} onChange={updateSearch}/> 
+      <InputLabel htmlFor="tags">Filtrar por Region</InputLabel>
+      <input type="text" value={search} onChange={updateSearch} />
+      <InputLabel htmlFor="tags">
+        Filtrar por Categoria Agro / Ganaderia
+      </InputLabel>
+      <input type="text" value={search2} onChange={updateSearch2} />
       {filteredTips.map((element, index) => {
         return (
           <div key={index} style={{ dispaly: "flex" }}>
